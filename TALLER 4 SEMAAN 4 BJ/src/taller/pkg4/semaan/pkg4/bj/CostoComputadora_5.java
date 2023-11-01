e1ef76c894348a3892471f25c8449995e109f59e
Joki02 <bjalao@utpl.edu.ec>
2023-11-01 17:04:17 -0500

    Clases

---
+++ TALLER 4 SEMAAN 4 BJ/src/taller/pkg4/semaan/pkg4/bj/CostoComputadora_5.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller.pkg4.semaan.pkg4.bj;
+
+/**
+ *
+ * @author SALAI
+ */
+public class CostoComputadora_5 {
+    
+}
---
+++ TALLER 4 SEMAAN 4 BJ/src/taller/pkg4/semaan/pkg4/bj/GastoHijos_2.java
@@ -0,0 +1,22 @@
+/*
+Generar un algoritmo que permite ingresar los gastos de tres hijos de un padre de
+familia; calcular y mostrar el total de gastos de los hijos del padre de familia.
+*/
+package taller.pkg4.semaan.pkg4.bj;
+import java.util.Scanner;
+public class GastoHijos_2 {
+
+    public static void main(String[] args) {
+        double gastosHijo1, gastosHijo2, gastosHijo3, gastosTotales;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese los gastos del hijo 1");
+        gastosHijo1 = teclado.nextDouble();
+        System.out.println("Ingrese los gastos del hijo 2");
+        gastosHijo2 = teclado.nextDouble();
+        System.out.println("Ingrese los gastos del hijo 3");
+        gastosHijo3 = teclado.nextDouble();
+        gastosTotales = gastosHijo1 + gastosHijo2 + gastosHijo3;
+        System.out.println("LOS GASTOS TOTALES DE LOS 3 HIJOS SON:"+ gastosTotales);
+
+    }
+}
